ddecd451940257528cace8b6fe4292293a5bdc1a
ez-li <eileenli.sf@gmail.com>
2019-07-29 21:24:18 -0700

    Complete VideoListEntry and VideoPlayer specs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import VideoList from './components/VideoList.js';
+import VideoListEntry from './components/VideoListEntry.js';
+
+import exampleVideoData from './data/exampleVideoData.js';
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
